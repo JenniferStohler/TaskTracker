8f479946ab16cc20bcbf1328b1ec084e0311adc1
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-06-28 13:21:50 -0600

    Tasks done

---
+++ src/components/Task.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="task">
+  <h3>{{ task.text }}</h3>
+  <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'task',
+  props: {
+    task: Object
+  }
+}
+</script>
